8c139210db997c6f0a6d6d78d235d8e4c4b1a798
Helene <hhoffman2@hawkmail.hccfl.edu>
2019-04-27 02:39:58 -0400

    Testing Git

---
+++ TestGitProject/src/testgitproject/TestGitProject.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgitproject;
+
+/**
+ *
+ * @author Helene
+ */
+public class TestGitProject {
+
+    /**
+     * public
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Testing one two one two is thing on");
+    }
+    
+}
